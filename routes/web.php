8f9e9e20ee663aa9c1175018c984c513a71c9901
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-28 22:07:10 +0530

    :sparkles: Create route paths of application

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/blog/{post_slug?}', [BlogController::class, 'blogDetail'])->name('blog');
+ 
+Route::post('/like', [BlogController::class, 'fetchLike']);
+Route::post('/like/{id}', [BlogController::class, 'handleLike']);
+ 
+Route::post('/dislike', [BlogController::class, 'fetchDislike']);
+Route::post('/dislike/{id}', [BlogController::class, 'handleDislike']);
