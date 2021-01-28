61dfee9add86296aa396d3fc020fd03d6cf72d4c
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-28 15:30:35 +0530

    :hammer: Configure blog seeder using faker

---
+++ database/seeders/BlogSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+    
+        Blog::create([
+            'post_title' => $this->faker->unique()->sentence,
+            'description' => $this->faker->paragraph,
+            'post_slug' => $this->faker->unique()->sentence,
+            'user_id' => 1,
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
+    }
+}
