3aa839f029b3fa7f3f201639543dfe95726aaf76
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-29 15:44:18 +0530

    :hammer: Replace the new hcode for the new layout of the application

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -1,20 +1,31 @@
 @extends('layout.app')
 
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Blog Detail</div>
 
-                <div class="card-body">
-                    <h2>{{ $blog->post_title }}</h2>
+    <section class="tm-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9 col-xl-9">
+                    <div class="tm-blog-post">
+                        <h3 class="tm-gold-text">{{ $blog->post_title }}</h3>
+                        <img src="{{ asset('img/tm-img-1010x336-1.jpg') }}" alt="Image" class="img-fluid tm-img-post">
 
-                    <like-component :blog="{{ $blog->id }}"></like-component>
-                    <dislike-component :blog="{{ $blog->id }}"></dislike-component>
+                        <p>{{ $blog->description }}</p>
+                        <div class="row">
+                            <div class="col">
+                                <like-component :blog="{{ $blog->id }}"></like-component>
+                            </div>
+                            <div class="col">
+                                <dislike-component :blog="{{ $blog->id }}"></dislike-component>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
-    </div>
-</div>
+    </section>
+
 @endsection
+
