221a9ba8007bdae667d5a212ec3f066568f482bd
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-29 15:46:46 +0530

    :hammer: Replace the new hcode for the new app layout of the application

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,12 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>Laravel Vue JS Like Dislike Demo</title>
+
+    
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/templatemo-style.css') }}">
 
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+
 
 </head>
 <body>
@@ -20,5 +25,10 @@
         </main>
 
     </div>
+
+    <script src="{{ asset('js/jquery-1.11.3.min.js') }}"></script>
+    <script src="{{ asset('https://www.atlasestateagents.co.uk/javascript/tether.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
 </body>
 </html>
