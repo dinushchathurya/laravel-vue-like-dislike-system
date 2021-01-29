35d810903eced295b06f6ab1406647f690f998c1
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-29 09:04:12 +0530

    :hammer: Change main entry point to blog layout

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/blog', [BlogController::class, 'index']);
-Route::get('/blog/{post_slug?}', [BlogController::class, 'blogDetail'])->name('blog');
+Route::get('/', [BlogController::class, 'index']);
+Route::get('/{post_slug?}', [BlogController::class, 'blogDetail'])->name('blog');
  
 Route::post('/like', [BlogController::class, 'fetchLike']);
 Route::post('/like/{id}', [BlogController::class, 'handleLike']);
