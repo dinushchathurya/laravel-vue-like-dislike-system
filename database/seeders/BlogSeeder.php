d31be520f343075f6a48f20354d16bb8b82b3512
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-29 15:41:34 +0530

    :art: Remove default comments from file

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Seeder;
 
 class BlogSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run(){
     
         Blog::create([
